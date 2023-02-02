b560d2e3627814f6abc11800d80d26f24ef884d1
elcomandamahmoud <elcomandamahmoud2011@gmail.com>
2023-02-02 17:30:22 +0200

    Add project files.

---
+++ Asignment16/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Asignment16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            Regex regName = new Regex("^[a-z]*$");
+            Regex regAge= new Regex("^\\d\\d$");
+            Regex regPhone= new Regex("^\\d{11}$");
+            string name = TXTName.Text.Trim();
+            string age= TXTAge.Text.Trim();
+            string phone= TXTPhone.Text.Trim();
+            if (regName.IsMatch(name)&&regAge.IsMatch(age)&&regPhone.IsMatch(phone))
+            {
+                MessageBox.Show("Welcome "+name+", your age is: "+age+", and your phone is: "+phone);
+            }
+        }
+    }
+}
+g
